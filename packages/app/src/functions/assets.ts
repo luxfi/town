492a61e381755d5a8da0231dafc3e6cfec8de2d8
corsega <dev@cosega.io>
2021-10-06 07:29:48 -0500

    modal progress

--- packages/app/src/functions/assets.ts
+++ packages/app/src/functions/assets.ts
@@ -1,4 +1,4 @@
-const ASSET_NODE = 'Node'
+const ASSET_VALIDATOR = 'Validator'
 const ASSET_ATM = 'ATM'
 const ASSET_WALLET = 'Genesis Wallet'
 const ASSET_CASH = 'Cash'
@@ -13,7 +13,7 @@ export const newAsset = (tokenId, type, props = {}) => {
 
 export const getOwnedAssets = () => {
   return [
-    newAsset(1, ASSET_NODE),
+    newAsset(1, ASSET_VALIDATOR),
     newAsset(2, ASSET_ATM),
     newAsset(3, ASSET_WALLET),
     newAsset(4, ASSET_WALLET),
