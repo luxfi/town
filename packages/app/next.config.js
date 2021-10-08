9fa11bb6c5f28a6b25176e1f10b78fdd5eac464a
Zach Kelling <z@zeekay.im>
2021-10-08 04:19:01 +0200

    Add netlify config

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -17,6 +17,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const { withSentryConfig } = require('@sentry/nextjs')
 
 const nextConfig = {
+  target: 'serverless',
   webpack: (config) => {
     config.module.rules = [
       ...config.module.rules,
@@ -38,133 +39,6 @@ const nextConfig = {
     domains: ['assets.sushi.com', 'res.cloudinary.com', 'raw.githubusercontent.com', 'logos.covalenthq.com'],
   },
   reactStrictMode: true,
-  async redirects() {
-    return [
-      // {
-      //   source: '/',
-      //   destination: '/swap',
-      //   permanent: true,
-      // },
-
-      {
-        source: '/zap',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/yield',
-        destination: '/farm',
-        permanent: true,
-      },
-      {
-        source: '/bento',
-        destination: '/bentobox',
-        permanent: true,
-      },
-      {
-        source: '/bento/kashi',
-        destination: '/lend',
-        permanent: true,
-      },
-      // Analytics
-      {
-        source: '/analytics',
-        destination: '/analytics/dashboard',
-        permanent: true,
-      },
-      {
-        source: '/portfolio',
-        destination: '/analytics/portfolio',
-        permanent: true,
-      },
-    ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/stake',
-        destination: '/bar',
-      },
-      {
-        source: '/add/:token*',
-        destination: '/exchange/add/:token*',
-      },
-      {
-        source: '/remove/:token*',
-        destination: '/exchange/remove/:token*',
-      },
-      {
-        source: '/create/:token*',
-        destination: '/exchange/add/:token*',
-      },
-      {
-        source: '/swap',
-        destination: '/exchange/swap',
-      },
-      {
-        source: '/swap/:token*',
-        destination: '/exchange/swap/:token*',
-      },
-      {
-        source: '/limit-order',
-        destination: '/exchange/limit-order',
-      },
-      {
-        source: '/limit-order/:token*',
-        destination: '/exchange/limit-order/:token*',
-      },
-      {
-        source: '/open-order',
-        destination: '/exchange/open-order',
-      },
-      {
-        source: '/migrate',
-        destination: '/exchange/migrate',
-      },
-      {
-        source: '/pool',
-        destination: '/exchange/pool',
-      },
-      {
-        source: '/find',
-        destination: '/exchange/find',
-      },
-      // Kashi
-      {
-        source: '/borrow',
-        destination: '/kashi/borrow',
-      },
-      {
-        source: '/borrow/:token*',
-        destination: '/kashi/borrow/:token*',
-      },
-      {
-        source: '/lend',
-        destination: '/kashi/lend',
-      },
-      {
-        source: '/lend/:token*',
-        destination: '/kashi/lend/:token*',
-      },
-      // Onsen
-      // {
-      //   source: '/farm',
-      //   destination: '/onsen',
-      // },
-      // {
-      //   source: '/farm/:type*',
-      //   destination: '/onsen/:type*',
-      // },
-      {
-        source: '/me',
-        destination: '/user',
-      },
-      {
-        source: '/balances',
-        destination: '/user/balances',
-      },
-    ]
-  },
   i18n: {
     localeDetection: true,
     locales,
