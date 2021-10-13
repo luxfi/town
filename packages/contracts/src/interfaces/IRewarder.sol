507998e38f93037197ff7b30de1dea110577de06
bulcaedion <85526510+bulcaedion@users.noreply.github.com>
2021-10-12 23:48:49 -0500

    support erc20 currencies

--- packages/contracts/src/interfaces/IRewarder.sol
+++ packages/contracts/src/interfaces/IRewarder.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.6.12;
+pragma solidity ^0.8.4;
 
-import '@luxdefi/solidity/contracts/interfaces/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 interface IRewarder {
   function onTokensReward(
