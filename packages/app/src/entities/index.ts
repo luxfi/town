ac3493a38b0d642a53c9f9b617b5f75d757c425b
johneyo <johneyo57@yahoo.com>
2021-12-25 18:42:05 +0000

    languages

--- packages/app/src/entities/index.ts
+++ packages/app/src/entities/index.ts
@@ -6,7 +6,22 @@ export { default as BigNumberMath } from './BigNumberMath'
 export { default as KashiCooker } from './KashiCooker'
 export { ChainlinkOracle, SushiSwapTWAP0Oracle, SushiSwapTWAP1Oracle } from './Oracle'
 import * as plurals from "make-plural/plurals";
-import { en, es, sr } from 'make-plural/plurals'
+import {
+    sr, de,
+    en,
+    es,
+    it,
+    ro,
+    ru,
+    vi,
+    zh,
+    ko,
+    ja,
+    fr,
+    fa,
+    pt,
+    hi,
+} from 'make-plural/plurals'
 export async function loadTranslation(locale: string, sessionId: string, isProduction = true) {
     console.log('hit me nw')
     let data
@@ -48,7 +63,20 @@ export async function loadTranslation(locale: string, sessionId: string, isProdu
 export function initTranslation(i18n: I18n): void {
     i18n.loadLocaleData({
         en: { plurals: en },
+        de: { plurals: de },
         es: { plurals: es },
+        it: { plurals: it },
+        ro: { plurals: ro },
+        ru: { plurals: ru },
+        vi: { plurals: vi },
+        zh_CN: { plurals: zh },
+        zh_TW: { plurals: zh },
+        ko: { plurals: ko },
+        pt_BR: { plurals: pt },
+        ja: { plurals: ja },
+        fa: { plurals: fa },
+        fr: { plurals: fr },
+        hi: { plurals: hi },
         pseudo: { plurals: en }
     })
 }
