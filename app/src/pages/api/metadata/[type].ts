e1637e1df093db913b758f6819819068d711caa8
Zach Kelling <z@zeekay.io>
2023-01-17 02:42:13 -0600

    Update API

--- app/src/pages/api/metadata/[type].ts
+++ app/src/pages/api/metadata/[type].ts
@@ -2,15 +2,16 @@ import { withSentry } from '@sentry/nextjs'
 
 const metadata = {
   description: 'Lux NFTs give you access to the Lux network at launch.',
-  external_url: 'https://app.lux.market/0x31e0f919c67cedd2bc3e294340dc900735810311',
+  external_url: 'https://app.lux.market/0x31e0f919c67cedd2bc3e294340dc900735810311/1',
   image: 'https://lux.town/nfts/validator.jpg',
-  animation_url: 'https://lux.town/nfts/validator.mp4'
+  animation_url: 'https://lux.town/nfts/validator.mp4',
   name: 'Validator',
   attributes: [],
 }
 
 const handler = async (req, res) => {
-  // const type = req.query.type.replace('.json')
+  const type = req.query.type.replace('.json')
+  console.log(type)
   // switch (type) {
   //   case 'validator': {
 
