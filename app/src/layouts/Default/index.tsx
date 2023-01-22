c6e12808d196e74355845c95fddb25bc331a74c6
Zach Kelling <z@zeekay.io>
2023-01-21 21:41:38 -0600

    Mint only

--- app/src/layouts/Default/index.tsx
+++ app/src/layouts/Default/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import Banner from '../../components/Banner'
 import Footer from '../../components/Footer'
-import Header from '../../components/Header'
+// import Header from '../../components/Header'
 import Main from '../../components/Main'
 import Popups from '../../components/Popups'
 import { useActiveWeb3React } from '../../hooks'
@@ -15,7 +15,7 @@ const Layout = ({ children, banner = undefined }) => {
   return (
     <div className="z-0 flex flex-col items-center w-full h-screen pb-16 lg:pb-0">
       {banner && <Banner />}
-      <Header />
+      {/*/<Header />*/}
       <Main>{children}</Main>
       <Popups />
       <Footer />
